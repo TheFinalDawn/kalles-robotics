e147223e03b16742655c89af3064c95227629b40
TheFinalDawn <adrian.waite@students.puyallupsd.org>
2019-01-17 12:08:37 -0800

    fixed typo

--- random python/calculation.py
+++ random python/calculation.py
@@ -1,4 +1,4 @@
-# Program make a simple calculator that can add, subtract, multiply and divide using functions
+# Program make a simple calculator that can add, subtract, multiply, divide, exponent, floor, and modulus using functions
 
 # This function adds two numbers
 def add(x, y):

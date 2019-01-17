219ce0020a24fd63dfe24eeb47d10e0071d6d150
TheFinalDawn <adrian.waite@students.puyallupsd.org>
2019-01-17 10:29:44 -0800

    Log Dump

---
+++ random python/input.py
@@ -0,0 +1,15 @@
+print("Name 2 values...")
+print("x:")
+x = input()
+print("y:")
+y = input()
+if y > x:
+    print("y is more than x.")
+elif x == y:
+    print("the two are the same.")
+elif x > y:
+    print("x is more than y.")
+else:
+    print("You broke it...")
+print("press enter...")
+a = input()
